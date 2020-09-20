6e2aa9af9139fcd63875d806b466dafc333a0aba
Stefan Baumgartner <sbaumgartner75@users.noreply.github.com>
2020-09-20 12:23:21 +0200

    Create create-users.sh

---
+++ bin/create-users.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+test -f /etc/users.list || exit 0
+
+while read id username hash groups; do
+        grep $username /etc/passwd && continue
+        # Create group
+        addgroup --gid $id $username
+        # Create user
+        useradd -m -u $id -s /bin/bash -g $username $username
+        # Set password
+        echo "$username:$hash" | /usr/sbin/chpasswd -e
+        # Add supplemental groups
+        usermod -a -G $groups $username
+done < /etc/users.list
